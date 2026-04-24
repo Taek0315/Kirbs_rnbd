37a413e6f9183bb46dc09882048eef2c5d9cc468
TawnyOwl <toh315@gmail.com>
2026-04-24 09:57:55 +0900

    Update search.py

--- career_demo/search.py
+++ career_demo/search.py
@@ -1462,9 +1462,9 @@ def inject_css() -> None:
             display:flex;
             flex-wrap:wrap;
             gap:8px;
-            height:100px;
-            min-height:100px;
-            max-height:100px;
+            height:105px;
+            min-height:105px;
+            max-height:105px;
             overflow:hidden;
             margin-bottom:14px;
             padding-left:4px;
